5ef7b84634b967582e71e342b70bc76646ab4cbd
mystic <215104920@qq.com>
2019-11-21 21:41:23 +0800

    a little bit change

--- sw.js
+++ sw.js
@@ -12,6 +12,12 @@
  */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
+// Detailed logging is very useful during development
+workbox.setConfig({debug: true});
+
+// Updating SW lifecycle to update the app after user triggered refresh
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
