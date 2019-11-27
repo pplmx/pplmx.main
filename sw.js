c6757a2d4ec2ab9d57fbf500fefa9c65cf81a737
mystic <215104920@qq.com>
2019-11-27 08:39:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,35 +39,72 @@ self.addEventListener("activate", async (event) => {
     return self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-    // console.log("[Service Worker] Fetched resource ", event.request.url);
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
     // Fix the following error:
     // Uncaught (in promise) TypeError: Failed to execute 'fetch' on 'WorkerGlobalScope': 'only-if-cached' can be set only with 'same-origin' mode
     if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
         console.log('Fetching operation throws a exception: ', event);
         return;
     }
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            // response from cache
-            if (response.status === 200) {
-                return response;
-            }
 
-            // response from network
-            fetch(event.request).then(resp => {
-                if (resp.status === 200) {
-                    return caches.open(pre_cache_name).then(cache => {
-                        cache.put(event.request, resp.clone());
-                        return resp;
-                    });
+    // if range, to handle partial content, whose status code is 206
+    if (event.request.headers.get('range')) {
+        var pos =
+            Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
+        console.log('Range request for', event.request.url,
+            ', starting position:', pos);
+        event.respondWith(
+            caches.open(CURRENT_CACHES.prefetch)
+                .then(function (cache) {
+                    return cache.match(event.request.url);
+                }).then(async function (res) {
+                    if (!res) {
+                        const res_1 = await fetch(event.request);
+                        return res_1.arrayBuffer();
+                    }
+                    return res.arrayBuffer();
+                }).then(function (ab) {
+                    return new Response(
+                        ab.slice(pos),
+                        {
+                            status: 206,
+                            statusText: 'Partial Content',
+                            headers: [
+                                // ['Content-Type', 'video/webm'],
+                                ['Content-Range', 'bytes ' + pos + '-' +
+                                    (ab.byteLength - 1) + '/' + ab.byteLength]]
+                        });
+                }));
+    } else {
+        console.log('Non-range request for', event.request.url);
+        event.respondWith(
+            // caches.match() will look for a cache entry in all of the caches available to the service worker.
+            // It's an alternative to first opening a specific named cache and then matching on that.
+            caches.match(event.request).then(function (response) {
+                if (response) {
+                    console.log('Found response in cache:', response);
+                    return response;
                 }
-            });
-        }).catch(error => {
-            console.log("Something wrong occurred: ", error);
-        })
-    );
+                console.log('No response found in cache. About to fetch from network...');
+                // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+                // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+                return fetch(event.request).then(function (response) {
+                    console.log('Response from network is:', response);
+                    return response;
+                }).catch(function (error) {
+                    // This catch() will handle exceptions thrown from the fetch() operation.
+                    // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+                    // It will return a normal response object that has the appropriate error code set.
+                    console.error('Fetching failed:', error);
+                    throw error;
+                });
+            })
+        );
+    }
 });
+
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
